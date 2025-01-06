d15cbc362d7e0bf3591ebd25f2f79719b7ba00a2
Kirana Trupthi Shree <kiranats2006@gmail.com>
2025-01-06 22:57:23 +0530

    Mongodb connection added for homepage

--- server.js
+++ server.js
@@ -5,15 +5,27 @@ const connectDatabase = require('./DB/database');
 if (process.env.NODE_ENV !== 'PRODUCTION') {
     require('dotenv').config();
   }
+let dbConnectionStat= 'Not connected';
 
+const updatedConnectionDatabase= async()=>{
+    try {
+        const connectionData=await connectDatabase();
+        dbConnectionStat=`Database is connected successfully`
+    } catch (error) {
+        dbConnectionStat = `Database connection failed: ${error.message}`;
+    }
+}
 
 const pingResponseMessage = 'Bwahahah!';
 
 app.get('/ping', (req, res) => {
     res.send(pingResponseMessage);
 });
+app.get('/', (req, res) => {
+    res.send(`Database Connection Status: ${dbConnectionStat}`);
+});
 
 app.listen(port, () => {
-    connectDatabase();
+    updatedConnectionDatabase();
     console.log(`Server is running on localhost:${port}`);
 });
