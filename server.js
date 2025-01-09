279198369e1e25bccf52ec2eef48dca243f89ec5
Kirana Trupthi Shree <kiranats2006@gmail.com>
2025-01-09 17:40:40 +0530

    CRUD operations added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const connectDatabase = require('./DB/database');
-// const getDb=require('./DB/mongo-client')
+const getDb=require('./DB/mongo-client')
 app.use(express.json());
 
 if (process.env.NODE_ENV !== 'PRODUCTION') {
@@ -26,45 +26,45 @@ app.get('/ping', (req, res) => {
 app.get('/', (req, res) => {
     res.send(`Database Connection Status: ${dbConnectionStat}`);
 });
-// app.get('/user', async (req, res)=>{
-//     try {
-//         const db=await getDb()
-//         const userData= await db.find().toArray();
-//         return res.status(200).send(userData)
-//     } catch (error) {
-//         return res.status(500).send({message: error.message})
-//     }
-// })
-// app.post('/', async(req,res)=>{
-//     try {
-//         const db=await getDb()
-//         const insertData= await db.insertOne({...req.body})
-//         return res.status(201).send({message: 'Created user',insertData })
-//     } catch (error) {
-//         return res.status(500).send({message: error.message}) 
-//     }
-// })
-// app.delete('/:id',async (req,res)=>{
-//     try {
-//         const db=await getDb()
-//         const {id}= req.params;
-//         const deleteUser=await db.findOneAndDelete({_id: id})
-//         return res.status(200).send({message:'Deleted successfully', deleteUser})
+app.get('/user', async (req, res)=>{
+    try {
+        const db=await getDb()
+        const userData= await db.find().toArray();
+        return res.status(200).send(userData)
+    } catch (error) {
+        return res.status(500).send({message: error.message})
+    }
+})
+app.post('/', async(req,res)=>{
+    try {
+        const db=await getDb()
+        const insertData= await db.insertOne({...req.body})
+        return res.status(201).send({message: 'Created user',insertData })
+    } catch (error) {
+        return res.status(500).send({message: error.message}) 
+    }
+})
+app.delete('/:id',async (req,res)=>{
+    try {
+        const db=await getDb()
+        const {id}= req.params;
+        const deleteUser=await db.findOneAndDelete({_id: id})
+        return res.status(200).send({message:'Deleted successfully', deleteUser})
         
-//     } catch (error) {
-//         return res.status(500).send({message: error.message})
-//     }
-// })
-// app.put('/:id', async (req,res)=>{
-//     try {
-//         const db=await getDb()
-//         const {id}=req.params;
-//         const updateuser= await db.findOneAndUpdate({_id: id}, {$set: req.body}, { returnDocument: 'after' });
-//         return res.status(200).send({message: 'Updated successfully',updateuser})
-//     } catch (error) {
-//         return res.status(500).send({message: error.message})
-//     }
-// })
+    } catch (error) {
+        return res.status(500).send({message: error.message})
+    }
+})
+app.put('/:id', async (req,res)=>{
+    try {
+        const db=await getDb()
+        const {id}=req.params;
+        const updateuser= await db.findOneAndUpdate({_id: id}, {$set: req.body}, { returnDocument: 'after' });
+        return res.status(200).send({message: 'Updated successfully',updateuser})
+    } catch (error) {
+        return res.status(500).send({message: error.message})
+    }
+})
 
 app.listen(port, () => {
     updatedConnectionDatabase();
