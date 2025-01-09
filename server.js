6a2de0149f12aa0e7ae7bf0bbf1e01d851fe23ea
Kirana Trupthi Shree <kiranats2006@gmail.com>
2025-01-09 18:35:46 +0530

    FINALL

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const connectDatabase = require('./DB/database');
-const {getDb, connection}=require('./DB/mongo-client')
+const {connection}=require('./DB/mongo-client')
 app.use(express.json());
 
 if (process.env.NODE_ENV !== 'PRODUCTION') {
     require('dotenv').config();
   }
-
-const pingResponseMessage = 'Bwahahah!';    
+  const pingResponseMessage = 'Bwahahah!';    
 app.get('/ping', (req, res) => {
     res.send(pingResponseMessage);
 });
 app.get('/', async (req, res) => {
   try {
-    // Check the database connection status
     const checkStatus = await connection.connect();
     const readyState = connection.topology.isConnected()
       ? 'connected'
@@ -28,45 +25,7 @@ app.get('/', async (req, res) => {
     res.status(500).send({ message: error.message });
   }
 });
-app.get('/user', async (req, res)=>{
-    try {
-        const db=await getDb()
-        const userData= await db.find().toArray();
-        return res.status(200).send(userData)
-    } catch (error) {
-        return res.status(500).send({message: error.message})
-    }
-})
-app.post('/', async(req,res)=>{
-    try {
-        const db=await getDb()
-        const insertData= await db.insertOne({...req.body})
-        return res.status(201).send({message: 'Created user',insertData })
-    } catch (error) {
-        return res.status(500).send({message: error.message}) 
-    }
-})
-app.delete('/:id',async (req,res)=>{
-    try {
-        const db=await getDb()
-        const {id}= req.params;
-        const deleteUser=await db.findOneAndDelete({_id: id})
-        return res.status(200).send({message:'Deleted successfully', deleteUser})
-        
-    } catch (error) {
-        return res.status(500).send({message: error.message})
-    }
-})
-app.put('/:id', async (req,res)=>{
-    try {
-        const db=await getDb()
-        const {id}=req.params;
-        const updateuser= await db.findOneAndUpdate({_id: id}, {$set: req.body}, { returnDocument: 'after' });
-        return res.status(200).send({message: 'Updated successfully',updateuser})
-    } catch (error) {
-        return res.status(500).send({message: error.message})
-    }
-})
+app.use('/user-router', require('./routes/user.route'))
 
 app.listen(port, () => {
     console.log(`Server is running on localhost:${port}`);
