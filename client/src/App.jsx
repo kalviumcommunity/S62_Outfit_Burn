f07dae7b86e1419f1735f3a8ea14a89bf66dd72f
Kirana Trupthi Shree <kiranats2006@gmail.com>
2025-01-13 12:15:55 +0530

    Added react to the repo

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Outfit Burn</h1>
+        <p>Outfit Burn is a fun platform for people to roast different outfits</p>
+        <p>Stay tuned for more features!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
